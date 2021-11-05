29b4a69da92f2f632bc7ccd24f7ee7c5e54c7398
Georgy <gthayyil@tamu.edu>
2021-11-05 14:04:29 -0500

    make it better

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 import sys
 import logging
 app = Flask(__name__)
-
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
